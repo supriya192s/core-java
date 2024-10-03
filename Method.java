758eead91ac1fbdcbc37ab0b54688b6745a18fda
supriya192s <supriencess@gmail.com>
2024-10-03 10:29:30 +0530

    first program

---
+++ Method.java
@@ -0,0 +1,16 @@
+public class Method{
+public static void main(String[] args){
+file();
+mymethod();
+variable();
+}
+public static void file(){
+System.out.println("my method is running");
+}
+public static void mymethod(){
+System.out.println("my method is running");
+}
+public static void variable(){
+System.out.println("my roll number is 5");
+}
+}
